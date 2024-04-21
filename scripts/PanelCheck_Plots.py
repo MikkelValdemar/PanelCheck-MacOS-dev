f65cfc106a2e8bc42e06d8d05e6e3169f698faba
Mikkel Valdemar Koch <mikkelvaldemarkoch@gmail.com>
2024-04-21 11:01:43 +0200

    Trying to fix line plot. Still works

--- scripts/PanelCheck_Plots.py
+++ scripts/PanelCheck_Plots.py
@@ -1,6 +1,7 @@
 
 
 # Plots
+#from scripts.plots.Line_Plot import ReplicateLinePlotter
 #from scripts.plots.Line_Plot import ReplicateLinePlotter, AssessorLineOverviewPlotter, AssessorLinePlotter, \
 #    SampleLineOverviewPlotter, SampleLinePlotter
 # from rawData_Plot import *
